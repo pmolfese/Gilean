bacf6b6674c4cfde275147febe54c20ed229b51c
Peter Molfese <pmolfese@gmail.com>
2026-03-26 16:14:07 -0400

    Initial Commit

---
+++ Gilean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gilean
+//
+//  Created by Molfese, Peter  [E] on 3/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gilean/GileanApp.swift
@@ -0,0 +1,17 @@
+//
+//  GileanApp.swift
+//  Gilean
+//
+//  Created by Molfese, Peter  [E] on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct GileanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
